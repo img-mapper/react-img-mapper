a5499c97271a629c80161516d2bb6682c16e3c46
NishargShah <nishargshah3101@gmail.com>
2021-01-09 23:24:56 +0530

    added babel and webpack

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/lib/ImageMapper.js',
+  output: {
+    path: path.resolve(__dirname, 'lib'),
+    filename: 'ImageMapper.js',
+    publicPath: '/lib',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
+};
