dae2faee2a6481dd0ce08139ded705e1ef560532
Nisharg Shah <nishargshah3101@gmail.com>
2023-11-20 00:23:19 +0530

    feat: draw code improved

--- src/draw.ts
+++ src/draw.ts
@@ -1,82 +1,70 @@
-import type { CTX } from '@/types';
+import type { DrawChosenShape, DrawShape, GetShape } from '@/types';
 
-const drawRect = (
-  scaledCoords: number[],
-  fillColor: string,
-  lineWidth: number,
-  strokeColor: string,
-  ctx: CTX
-) => {
-  const [left, top, right, bot] = scaledCoords;
+const drawRect: DrawChosenShape = (area, ctx) => {
+  const { scaledCoords, fillColor, lineWidth, strokeColor } = area;
+  const [left, top, right, bottom] = scaledCoords;
+
   ctx.current.fillStyle = fillColor;
   ctx.current.lineWidth = lineWidth;
   ctx.current.strokeStyle = strokeColor;
-  ctx.current.strokeRect(left, top, right - left, bot - top);
-  ctx.current.fillRect(left, top, right - left, bot - top);
+  ctx.current.strokeRect(left, top, right - left, bottom - top);
+  ctx.current.fillRect(left, top, right - left, bottom - top);
+  return true;
 };
 
-const drawCircle = (
-  scaledCoords: number[],
-  fillColor: string,
-  lineWidth: number,
-  strokeColor: string,
-  ctx: CTX
-) => {
+const drawCircle: DrawChosenShape = (area, ctx) => {
+  const { scaledCoords, fillColor, lineWidth, strokeColor } = area;
+  const [left, top, right] = scaledCoords;
+
   ctx.current.fillStyle = fillColor;
   ctx.current.beginPath();
   ctx.current.lineWidth = lineWidth;
   ctx.current.strokeStyle = strokeColor;
-  ctx.current.arc(scaledCoords[0], scaledCoords[1], scaledCoords[2], 0, 2 * Math.PI);
+  ctx.current.arc(left, top, right, 0, 2 * Math.PI);
   ctx.current.closePath();
   ctx.current.stroke();
   ctx.current.fill();
+  return true;
 };
 
-const drawPoly = (
-  scaledCoords: number[],
-  fillColor: string,
-  lineWidth: number,
-  strokeColor: string,
-  ctx: CTX
-) => {
-  const newCoords = scaledCoords.reduce(
-    (a, v, i, s) => (i % 2 ? a : [...a, s.slice(i, i + 2)]),
-    []
-  );
-  // const first = newCoords.unshift();
+const drawPoly: DrawChosenShape = (area, ctx) => {
+  const { scaledCoords, fillColor, lineWidth, strokeColor } = area;
+  const groupCoords = scaledCoords.reduce<[number, number][]>((acc, val, index, array) => {
+    if (index % 2) return acc;
+    return [...acc, array.slice(index, index + 2)] as [number, number][];
+  }, []);
+
+  // const first = groupCoords.unshift();
   ctx.current.fillStyle = fillColor;
   ctx.current.beginPath();
   ctx.current.lineWidth = lineWidth;
   ctx.current.strokeStyle = strokeColor;
 
   // ctx.current.moveTo(first[0], first[1]);
-  newCoords.forEach(c => ctx.current.lineTo(c[0], c[1]));
+  groupCoords.forEach(([first, second]) => ctx.current.lineTo(first, second));
   ctx.current.closePath();
   ctx.current.stroke();
   ctx.current.fill();
+  return true;
 };
 
-const callingFn = (
-  shape: string,
-  scaledCoords: number[],
-  fillColor: string,
-  lineWidth: number,
-  strokeColor: string,
-  ctx: CTX
-): void | boolean => {
-  if (shape === 'rect') {
-    drawRect(scaledCoords, fillColor, lineWidth, strokeColor, ctx);
-    return true;
-  }
-  if (shape === 'circle') {
-    drawCircle(scaledCoords, fillColor, lineWidth, strokeColor, ctx);
-    return true;
-  }
-  if (shape === 'poly') {
-    drawPoly(scaledCoords, fillColor, lineWidth, strokeColor, ctx);
-    return true;
-  }
+const getShape: GetShape = shape => {
+  if (shape === 'rect') return drawRect;
+  if (shape === 'circle') return drawCircle;
+  if (shape === 'poly') return drawPoly;
   return false;
 };
 
-export default callingFn;
+const drawShape: DrawShape = (area, ctx) => {
+  const { shape, ...restArea } = area;
+  const shapeFn = getShape(shape);
+
+  if (shapeFn && ctx.current instanceof CanvasRenderingContext2D) {
+    const currentCtx = { current: ctx.current };
+    return shapeFn(restArea, currentCtx);
+  }
+
+  return false;
+};
+
+export default drawShape;
